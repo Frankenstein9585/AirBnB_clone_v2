9159ba381e3f565a6ebb1fa87e0ed0361bc64258
Frankenstein9585 <donny9585@gmail.com>
2023-04-19 08:22:58 +0100

    Modified console.py

--- models/review.py
+++ models/review.py
@@ -8,3 +8,12 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, obj_dict=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if obj_dict:
+            for k, v in obj_dict.items():
+                setattr(self, k, v)
+        else:
+            return
+
