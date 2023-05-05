62351c5e5f0a6a2f9c861c699c829eac8fd55f1a
Frankenstein9585 <donny9585@gmail.com>
2023-05-05 15:16:04 +0100

    Modified task 2 again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,17 +7,17 @@ import os
 env.hosts = ['<IP web-01>', 'IP web-02']
 
 
-# def do_pack():
-#     """pack function"""
-#     try:
-#         if not os.path.exists('versions'):
-#             os.mkdir('versions')
-#         date_str = datetime.now().strftime('%Y%m%d%H%M%S')
-#         file_path = 'versions/web_static_{}.tgz'.format(date_str)
-#         local('tar -cvzf {} web_static'.format(file_path))
-#         return file_path
-#     except:
-#         return None
+def do_pack():
+    """pack function"""
+    try:
+        if not os.path.exists('versions'):
+            os.mkdir('versions')
+        date_str = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_path = 'versions/web_static_{}.tgz'.format(date_str)
+        local('tar -cvzf {} web_static'.format(file_path))
+        return file_path
+    except:
+        return None
 
 
 def do_deploy(archive_path):
@@ -27,17 +27,17 @@ def do_deploy(archive_path):
             return False
         put(archive_path, '/tmp/')
         archive_folder = archive_path.split('.')[0].split('/')[-1]
-        sudo('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
-        sudo(f'tar -xzf /tmp/{archive_folder}.tgz -C /data/web_static/'
-             f'releases/{archive_folder}/')
-        sudo(f'rm /tmp/{archive_folder}.tgz')
-        sudo(f'mv /data/web_static/releases/{archive_folder}/web_static/*'
-             f' /data/web_static/releases/{archive_folder}')
-        sudo(f'rm -rf /data/web_static/releases/{archive_folder}/web_static/')
-        sudo('rm -rf /data/web_static/current')
-        sudo(f'ln -s /data/web_static/releases/{archive_folder} '
-             f'/data/web_static/current')
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
+        run(f'tar -xzf /tmp/{archive_folder}.tgz -C /data/web_static/'
+            f'releases/{archive_folder}/')
+        run(f'rm /tmp/{archive_folder}.tgz')
+        run(f'mv /data/web_static/releases/{archive_folder}/web_static/*'
+            f' /data/web_static/releases/{archive_folder}')
+        run(f'rm -rf /data/web_static/releases/{archive_folder}/web_static/')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{archive_folder} '
+            f'/data/web_static/current')
         print('New version deployed!')
         return True
-    except FileNotFoundError:
+    except:
         return False
