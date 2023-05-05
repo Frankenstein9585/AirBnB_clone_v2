6d323ba8237504014efb85b361421b6e16037837
Frankenstein9585 <donny9585@gmail.com>
2023-05-05 15:04:08 +0100

    Modified task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,17 +7,17 @@ import os
 env.hosts = ['<IP web-01>', 'IP web-02']
 
 
-def do_pack():
-    """pack function"""
-    try:
-        if not os.path.exists('versions'):
-            os.mkdir('versions')
-        date_str = datetime.now().strftime('%Y%m%d%H%M%S')
-        file_path = 'versions/web_static_{}.tgz'.format(date_str)
-        local('tar -cvzf {} web_static'.format(file_path))
-        return file_path
-    except:
-        return None
+# def do_pack():
+#     """pack function"""
+#     try:
+#         if not os.path.exists('versions'):
+#             os.mkdir('versions')
+#         date_str = datetime.now().strftime('%Y%m%d%H%M%S')
+#         file_path = 'versions/web_static_{}.tgz'.format(date_str)
+#         local('tar -cvzf {} web_static'.format(file_path))
+#         return file_path
+#     except:
+#         return None
 
 
 def do_deploy(archive_path):
@@ -28,13 +28,16 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         archive_folder = archive_path.split('.')[0].split('/')[-1]
         sudo('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
-        sudo(f'tar -xzf /tmp/{archive_folder}.tgz -C /data/web_static/releases/{archive_folder}/')
+        sudo(f'tar -xzf /tmp/{archive_folder}.tgz -C /data/web_static/'
+             f'releases/{archive_folder}/')
         sudo(f'rm /tmp/{archive_folder}.tgz')
-        sudo(f'mv /data/web_static/releases/{archive_folder}/web_static/* /data/web_static/releases/{archive_folder}')
+        sudo(f'mv /data/web_static/releases/{archive_folder}/web_static/*'
+             f' /data/web_static/releases/{archive_folder}')
         sudo(f'rm -rf /data/web_static/releases/{archive_folder}/web_static/')
         sudo('rm -rf /data/web_static/current')
-        sudo(f'ln -s /data/web_static/releases/{archive_folder} /data/web_static/current')
+        sudo(f'ln -s /data/web_static/releases/{archive_folder} '
+             f'/data/web_static/current')
         print('New version deployed!')
         return True
-    except:
+    except FileNotFoundError:
         return False
