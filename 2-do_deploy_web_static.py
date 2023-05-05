9782140c19cc2ce5e4447bc6a355463358bd132b
Frankenstein9585 <donny9585@gmail.com>
2023-05-05 14:54:27 +0100

    Modified task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """Distributes the content of the archive to the web servers"""
-from fabric.api import sudo, put, env
+from fabric.api import *
+from datetime import datetime
 import os
 
-env.hosts = ['ubuntu@100.26.216.194']
+env.hosts = ['<IP web-01>', 'IP web-02']
+
+
+def do_pack():
+    """pack function"""
+    try:
+        if not os.path.exists('versions'):
+            os.mkdir('versions')
+        date_str = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_path = 'versions/web_static_{}.tgz'.format(date_str)
+        local('tar -cvzf {} web_static'.format(file_path))
+        return file_path
+    except:
+        return None
 
 
 def do_deploy(archive_path):
@@ -20,6 +34,7 @@ def do_deploy(archive_path):
         sudo(f'rm -rf /data/web_static/releases/{archive_folder}/web_static/')
         sudo('rm -rf /data/web_static/current')
         sudo(f'ln -s /data/web_static/releases/{archive_folder} /data/web_static/current')
+        print('New version deployed!')
         return True
     except:
         return False
